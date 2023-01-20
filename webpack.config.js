f78848e04e9593675dd2f6b21a263a9a2147d7b2
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2023-01-20 15:57:53 +0100

    Install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      title: 'Development',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
