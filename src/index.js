a81a5e7c836fbf872297ad02fc179aae91f6271e
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2023-01-20 15:31:19 +0100

    Instal Html and Css webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
