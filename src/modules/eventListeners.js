82ec5066e821ced146f7ed1c1654496b5824e48c
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2023-01-21 15:53:11 +0100

    Resolve linter errors

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,10 +1,10 @@
 import {
-    renderToDoList,
-    addTask,
-    clearCompleted,
-    editTask,
-    deleteTask,
-    markTask,
+  renderToDoList,
+  addTask,
+  clearCompleted,
+  editTask,
+  deleteTask,
+  markTask,
 } from './lists.js';
 import { updateLocalStorage } from './localStorage.js';
 import toDoTasks from '../tasks.js';
@@ -15,43 +15,43 @@ const clearCompletedBtn = document.querySelector('.clearbtn');
 const addTaskBtn = document.querySelector('.add_btn');
 
 input.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter' && input.value !== '') {
-        addTask(toDoTasks, input.value);
-        input.value = '';
-        updateLocalStorage(toDoTasks);
-        renderToDoList(toDoTasks);
-    }
+  if (e.key === 'Enter' && input.value !== '') {
+    addTask(toDoTasks, input.value);
+    input.value = '';
+    updateLocalStorage(toDoTasks);
+    renderToDoList(toDoTasks);
+  }
 });
 
 addTaskBtn.addEventListener('click', () => {
-    if (input.value !== '') {
-        addTask(toDoTasks, input.value);
-        input.value = '';
-        updateLocalStorage(toDoTasks);
-        renderToDoList(toDoTasks);
-    }
+  if (input.value !== '') {
+    addTask(toDoTasks, input.value);
+    input.value = '';
+    updateLocalStorage(toDoTasks);
+    renderToDoList(toDoTasks);
+  }
 });
 
 todoList.addEventListener('click', (e) => {
-    if (e.target.closest('.todo-list-li-checkbox')) {
-        markTask(e, toDoTasks);
-    }
+  if (e.target.closest('.todo-list-li-checkbox')) {
+    markTask(e, toDoTasks);
+  }
 });
 
 todoList.addEventListener('click', (e) => {
-    if (e.target.closest('.todo-list-li-text')) {
-        editTask(e, toDoTasks);
-    }
+  if (e.target.closest('.todo-list-li-text')) {
+    editTask(e, toDoTasks);
+  }
 });
 
 todoList.addEventListener('click', (e) => {
-    if (e.target.closest('.todo-list-li-cross')) {
-        deleteTask(e, toDoTasks);
-    }
+  if (e.target.closest('.todo-list-li-cross')) {
+    deleteTask(e, toDoTasks);
+  }
 });
 
 clearCompletedBtn.addEventListener('click', () => {
-    const toDoList = clearCompleted(toDoTasks);
-    updateLocalStorage(toDoList);
-    renderToDoList(toDoList);
+  const toDoList = clearCompleted(toDoTasks);
+  updateLocalStorage(toDoList);
+  renderToDoList(toDoList);
 });
